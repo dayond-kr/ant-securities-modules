442ebf7ebe0cce20558c982bbeb2b40edb76be78
SANGWOO PARK <prophet0915@gmail.com>
2025-09-13 01:00:45 +0900

    implement conversation history persistence & retrieval

---
+++ Ant.Modules/Utilities/OpenAI/Tool.cs
@@ -0,0 +1,16 @@
+﻿using OpenAI.Chat;
+
+namespace ShareInvest.Utilities.OpenAI;
+
+public static class Tool
+{
+    public static ChatTool GetDailyChartCandlesticks(byte[] bytes, string description)
+    {
+        return ChatTool.CreateFunctionTool(nameof(GetDailyChartCandlesticks), functionParameters: new BinaryData(bytes), functionDescription: description);
+    }
+
+    public static ChatTool GetStockItems(byte[] bytes, string description)
+    {
+        return ChatTool.CreateFunctionTool(nameof(GetStockItems), functionParameters: new BinaryData(bytes), functionDescription: description);
+    }
+}
