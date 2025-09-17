7756b28c587150dd1cede3cf3a5bb729de954c92
SANGWOO PARK <prophet0915@gmail.com>
2025-09-17 16:33:26 +0900

    implement responsive layout for desktop & mobile

--- Ant.Modules/GlobalSuppressions.cs
+++
@@ -1,3 +0,0 @@
-﻿using System.Diagnostics.CodeAnalysis;
-
-[assembly: SuppressMessage("Style", "IDE0130:네임스페이스가 폴더 구조와 일치하지 않습니다.", Justification = "<보류 중>", Scope = "namespace", Target = "~N:ShareInvest.Utilities")]
